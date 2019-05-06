e60780e791f80978ea7d3ed946094de0bfe192e2
OliveirarDan <oliveirardanilo@gmail.com>
2019-05-06 15:36:07 -0300

    subindo novo jar

---
+++ CadastraAlunos/src/main/java/CadastraAlunosApplication.java
@@ -0,0 +1,13 @@
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastraAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastraAlunosApplication.class, args);
+	}
+
+}
